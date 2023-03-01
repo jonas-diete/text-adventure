a14586224cef190034ac7773f3f7017b5ddbcd8a
Jonas Diete <jonas.diete@gmail.com>
2023-03-01 13:29:59 +0000

    added requirement for an action

--- TextAdventure/UserAction.cs
+++ TextAdventure/UserAction.cs
@@ -6,12 +6,17 @@
     public string resultDescription;
     public string result;
     public string resultAttribute;
-        public UserAction(string actionDescriptions, string actionResultDescription, string actionResult, string actionResultAttribute = "none")
+    public string requirement;
+    public string reqNotFulfilled;
+
+        public UserAction(string actionDescriptions, string actionResultDescription, string actionResult, string actionResultAttribute = "none", string actionRequirement = "none", string descriptionWhenReqNotFulfilled = "none")
         {
             descriptions = actionDescriptions;
             resultDescription = actionResultDescription;
             result = actionResult;
             resultAttribute = actionResultAttribute;
+            requirement = actionRequirement;
+            reqNotFulfilled = descriptionWhenReqNotFulfilled;
         }
     }
 }
