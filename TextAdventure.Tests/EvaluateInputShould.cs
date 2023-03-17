26950a3af9b04e6d654c5531d732cbf2000cc9dd
Jonas Diete <jonas.diete@gmail.com>
2023-03-17 16:33:34 +0000

    evaluate input tests for invalid user commands

--- TextAdventure.Tests/EvaluateInputShould.cs
+++ TextAdventure.Tests/EvaluateInputShould.cs
@@ -13,8 +13,8 @@ namespace TextAdventure.Tests
         {
             game = new Game();
             List<UserAction> actions = new List<UserAction>();
-            UserAction action1 = new UserAction("talk to troll;chat with troll;communicate with troll", "You say hi to the troll.", "none");
-            UserAction action2 = new UserAction("talk to knight;chat with knight;communicate with knight", "You say hi to the knight.", "none");
+            UserAction action1 = new UserAction("talk to troll;chat with troll;communicate with troll", "You say hi to the troll.");
+            UserAction action2 = new UserAction("talk to knight;chat with knight;communicate with knight", "You say hi to the knight.");
             actions.Add(action1);
             actions.Add(action2);
             Level level1 = new Level("level1", "Level Description Here", actions);
@@ -27,9 +27,25 @@ namespace TextAdventure.Tests
         [TestCase("talk to troll")]
         [TestCase("chat with troll")]
         [TestCase("communicate with troll")]
-        public void EvaluateInput_Should(string input)
+        public void EvaluateInput_ShouldReturnResultDescriptionOfFirstAction(string input)
         {
             Assert.That(game.EvaluateInput(input, game.currentLevel.actions), Is.EqualTo("You say hi to the troll."));
+        }
+
+        [TestCase("talk to knight")]
+        [TestCase("chat with knight")]
+        [TestCase("communicate with knight")]
+        public void EvaluateInput_ShouldReturnResultDescriptionOfSecondAction(string input)
+        {
+            Assert.That(game.EvaluateInput(input, game.currentLevel.actions), Is.EqualTo("You say hi to the knight."));
+        }
+
+        [TestCase("ask troll for the way")]
+        [TestCase("hit troll")]
+        [TestCase("fight the knight")]
+        public void EvaluateInput_ShouldReturnIDontUnderstand(string input)
+        {
+            Assert.That(game.EvaluateInput(input, game.currentLevel.actions), Is.EqualTo("I don't understand what to do."));
         }
     }
 }
