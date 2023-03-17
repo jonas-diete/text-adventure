20ee31e7d62be3123a2cf29d84c23d67b5f0e5da
Jonas Diete <jonas.diete@gmail.com>
2023-03-17 15:48:34 +0000

    get level tests all pass

--- TextAdventure.Tests/GetLevelShould.cs
+++ TextAdventure.Tests/GetLevelShould.cs
@@ -12,17 +12,26 @@ namespace TextAdventure.Tests
         public void SetUp()
         {
             game = new Game();
-        }
-
-        [Test]
-        public void GetLevel_ShouldReturnLevelDescriptionAndAskForInput()
-        {
             List<UserAction> actions = new List<UserAction>();
             Level level1 = new Level("level1", "Level Description Here", actions);
             List<Level> levels = new List<Level>();
             levels.Add(level1);
             game.levels = levels;
+        }
+
+        [Test]
+        public void GetLevel_ShouldReturnLevelDescriptionAndAskForInput()
+        {
             Assert.That(game.GetLevel("level1"), Is.EqualTo("\n\nLevel Description Here\nWhat do you want to do?"));
+            Assert.That(game.currentLevel.description, Is.EqualTo("Level Description Here"));
+        }
+
+        [Test]
+        public void GetLevel_ShouldReturnError()
+        {
+            game.GetLevel("level1");
+            Assert.That(game.GetLevel("level8"), Is.EqualTo("Error: Level doesn't exist."));
+            // current level didn't get updated:
             Assert.That(game.currentLevel.description, Is.EqualTo("Level Description Here"));
         }
     }
