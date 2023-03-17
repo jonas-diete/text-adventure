58ed971903f437a4e0732b517de51db99a353018
Jonas Diete <jonas.diete@gmail.com>
2023-03-17 16:28:01 +0000

    further evaluate input tests created

--- TextAdventure.Tests/EvaluateInputShould.cs
+++ TextAdventure.Tests/EvaluateInputShould.cs
@@ -13,8 +13,8 @@ namespace TextAdventure.Tests
         {
             game = new Game();
             List<UserAction> actions = new List<UserAction>();
-            UserAction action1 = new UserAction("talk to troll;chat with troll;shout at troll", "You say hi to the troll.", "none");
-            UserAction action2 = new UserAction("talk to knight;chat with knight;shout at knight", "You say hi to the knight.", "none");
+            UserAction action1 = new UserAction("talk to troll;chat with troll;communicate with troll", "You say hi to the troll.", "none");
+            UserAction action2 = new UserAction("talk to knight;chat with knight;communicate with knight", "You say hi to the knight.", "none");
             actions.Add(action1);
             actions.Add(action2);
             Level level1 = new Level("level1", "Level Description Here", actions);
@@ -24,10 +24,12 @@ namespace TextAdventure.Tests
             game.currentLevel = game.levels[0];
         }
 
-        [Test]
-        public void EvaluateInput_Should()
+        [TestCase("talk to troll")]
+        [TestCase("chat with troll")]
+        [TestCase("communicate with troll")]
+        public void EvaluateInput_Should(string input)
         {
-            Assert.That(game.EvaluateInput("talk to troll", game.currentLevel.actions), Is.EqualTo("You say hi to the troll."));
+            Assert.That(game.EvaluateInput(input, game.currentLevel.actions), Is.EqualTo("You say hi to the troll."));
         }
     }
 }
